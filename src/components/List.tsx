0e775dc231306871880ebcc88e9667ff72219ab1
GFD1998 <gabrieldennett@gmail.com>
2023-04-15 23:09:58 -0400

    General Update

---
+++ src/components/List.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ListStyles from '../styles/modules/List.module.css';
+
+
+export default function List({ children }){
+    console.log(children);
+    return (
+        <>
+            <div id={ListStyles.aboutContainer}>
+                {children}
+            </div>
+        </>
+    )
+}
+
+export function ListItem({ image, header, text }){
+    return(
+        <>
+            <div className='about-persona'>
+                <div className='about-image'>
+                    <img src={image} />
+                </div>
+                <div className='about-text'>
+                    <h1>{header}</h1>
+                    <p>
+                        {text}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    // <h1>Home</h1>
     <div id='home-container'>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac bibendum quam, vel finibus leo. Morbi mattis, odio sed fermentum convallis, dolor lorem porta magna, eu consequat lorem magna a turpis. Vivamus ultrices elit risus, at egestas enim tempus non. Vivamus molestie lacinia dui in tincidunt. Morbi vulputate accumsan scelerisque. Nunc justo turpis, blandit ac vestibulum congue, ullamcorper mattis orci. Mauris in libero id lectus pellentesque sollicitudin. Vivamus convallis, erat viverra congue porttitor, justo enim hendrerit eros, eget consequat velit sapien ac ligula. Duis ac porta nulla, non mattis ligula.
